44b4c91a620cc9d7e3f02d3c9d3b1ad83c5f9fdc
“Ihor <kempahe@gmail.com>
2023-04-05 13:17:31 +0300

    Firs second notebook

--- src/justfun/notebook.java
+++ src/justfun/notebook.java
@@ -12,35 +12,34 @@ public class notebook {
       noteBook = new TreeMap <>();
       scanner = new Scanner(System.in);
       notebook not = new notebook();
+      
       for(;;){
       System.out.println("Hello you in main menu"+"\n"+ "Please chose operantion");
        mainOperation = scanner.next();
-      if(mainOperation.equalsIgnoreCase(add)){
+    
+       if(mainOperation.equalsIgnoreCase(add)){
           Scanner addscanner = new Scanner(System.in);
           System.out.println("Input name");
           String name = addscanner.next();
           System.out.println("Input number");
           Integer number= addscanner.nextInt();
           not.addContact(name, number);  }
+    
       if(mainOperation.equalsIgnoreCase(remove)){
-          System.out.println("Make choose and enter you to remove by name or by number" );
+          System.out.println("Enter name or number to remove :" );
           Scanner removescanner = new Scanner(System.in);
-          String nameOrNumber =removescanner.next();
-          if (nameOrNumber.equalsIgnoreCase("name")){
-              Scanner removeName = new Scanner(System.in);
-              System.out.println("Input name :");
-          String name =removeName.next();
-          if (noteBook.containsKey(name)){
-         not.removeContactByName(name);
-              System.out.println("Removed :"+ name );}
-              System.out.println("There is no such contact !");
+          Object nameOrNumber =removescanner.next();
+         
+          if ( noteBook.containsKey(nameOrNumber.toString())){
+            not.removeContactByName(nameOrNumber.toString());
+              System.out.println("Contact :"+nameOrNumber.toString()+ " removed !");                 
           } 
-          else if (nameOrNumber.equalsIgnoreCase("number")){
-          Scanner removeNumber = new Scanner(System.in);
-          System.out.println("Input number :");
-          int number =removeNumber.nextInt();
-                not.removeContactByNumber(number);
-             System.out.println("Removed :"+ number );} }
+        Integer castObjectToNumber = Integer.parseInt(nameOrNumber.toString()) ;
+          if (noteBook.containsValue(castObjectToNumber)){
+                not.removeContactByNumber(castObjectToNumber);
+             System.out.println("Contdct :"+ castObjectToNumber+ " removed" );}
+          else {System.out.println("There is no such contact !");}}
+     
       if(mainOperation.equalsIgnoreCase(displayAllContact)){
           System.out.println("Your saved contacts :");
           not.displayAllContact();} }
@@ -60,7 +59,7 @@ public void addContact (String Name,Integer Number){
 public void removeContactByName (String Name){
     if (noteBook.containsKey(Name)){
       noteBook.remove(Name);
-        System.out.println("Contact :"+Name + " removed");
+        //System.out.println("Contact :"+Name + " removed");
     }
 }
 public void removeContactByNumber (Integer Number){
@@ -68,7 +67,7 @@ public void removeContactByNumber (Integer Number){
     for (Entry<String,Integer> entery :noteBook.entrySet()){
         if (entery.getValue().equals(Number)){
             noteBook.remove(entery.getKey());
-        System.out.println("Contact :"+Number +" removed");
+       // System.out.println("Contact :"+Number +" removed");
         }
     }
 }
